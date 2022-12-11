9428328efe148819299e142947c60df3df16a73e
Suraj sutar <66626903+suraj-sutar@users.noreply.github.com>
2022-12-11 05:50:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+# age = 20
+
+# is_over_age = age >= 18
+# print(is_over_age)
+# is_under_age = age < 18
+# is_twenty = age == 20
+
+# my_number = 5
+# user_number = int(input("Enter your number: "))
+
+# equal_or_not = my_number == user_number
+# print(f"You got it Right {equal_or_not}..")
+
+# age = int(input("enter your age:"))
+
+# can_learn_programming = age > 0 and age < 150
+# print(f"You can learn programming {can_learn_programming}...")
+
+# for example you are not working
+
+# age = int(input("Enter your age: "))
+# usually_not_working = age < 18 or age > 65
+
+# print(f"you are usually not working {usually_not_working}..")
+
+
+# age = 25
+# result = age > 18 and age < 65
+# print(result)
+
+# in and keyword true and true so answer is True if in condition one condition is false then it returns false
+
+# True  false  false
+# True  true    true
+# false  true  false
+# False  false  false
+
+# in and keyword first value is false it returns first value and if it first value is not false than it returns second value
+
+
+# or keyword is opsite to the and keyword
+# if the first value is true the it returns first value and if the first value is not true than it returns second value
+
+# age = 25
+# result = age > 18 or age < 65
+# print(result)
+
+# in or keyword 
+# true  true  true
+# false  true  true
+# true  false  true
+# false  false  false
+
+# x = True
+# cmp =  x and 18
+# print(cmp)
+
+age  = int(input("enter your age:"))
+side_job = True
+print(age > 18 and age < 65 or side_job)
+
